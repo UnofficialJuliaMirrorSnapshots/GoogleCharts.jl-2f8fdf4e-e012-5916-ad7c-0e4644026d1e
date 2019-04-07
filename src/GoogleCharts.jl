b6ce5c4c2bda0bdb0a1ad7b43076046a966ba5fb
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 21:49:34 +0000

    Snapshot of branch master taken on 2019-04-07T17:49:33.01-04:00 from "https://github.com/UnofficialJuliaMirror/GoogleCharts.jl-2f8fdf4e-e012-5916-ad7c-0e4644026d1e"

---
+++ src/GoogleCharts.jl
@@ -0,0 +1,40 @@
+
+
+module GoogleCharts
+
+using Mustache
+using JSON
+using DataArrays
+using DataFrames
+using Base.Dates
+using Compat
+
+import Base.show, Base.display
+
+
+
+include("utils.jl")
+include("charts.jl")
+include("render.jl")
+include("conveniences.jl")
+
+tpl_name = joinpath(dirname(@__FILE__),"..", "tpl", "chart.html")
+global chart_tpl = Mustache.template_from_file(tpl_name)
+
+
+
+
+export GoogleChart
+export help_on_chart
+export area_chart, bar_chart, bubble_chart, candlestick_chart, column_chart, combo_chart,
+       gauge_chart, geo_chart, line_chart, pie_chart, scatter_chart, stepped_area_chart,
+       table_chart, tree_chart, annotated_time_line, intensity_map, motion_chart, org_chart,
+       image_spark_line
+export Plot, Scatter, help_on_chart
+export Boxplot, Hist
+export Surfaceplot
+
+
+
+end ## module
+
